b88e01e8f89602016ec973dddb9d13b3084109b4
PRATIK-RAKTATE <rhutikraktate@gmail.com>
2025-09-05 15:43:27 +0530

    seo: add seo config

--- frontend/v1/src/Components/department/department.jsx
+++ frontend/v1/src/Components/department/department.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import Carousel from './HeroCarousel'
 import SidebarHod from './SidebarHod'
 import Departmentinfo from './Departmentinfo'
+import SEO from '../../seo/SEO';
+import SEO_CONFIG from '../../seo/seo.config';
 
 
 const department = ({slides,vision,mission,hodDesk,Hodimg,Hodname,hodQ,Department}) => {
+  const { title, description, url, image } = SEO_CONFIG.departments.Department|| {};
   return (
     <div className='flex flex-col'>
+      <SEO title={title} description={description} url={url} image={image} />
      <div>
         <Carousel slides={slides} />
       </div> 
