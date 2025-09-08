17b4c09f60f080ee386ed2c37026717835731df6
Yogesh chavan <yogeshchavan1209@gmail.com>
2025-09-08 14:31:45 +0530

    fix: department responsiveness

--- frontend/v1/src/Components/department/Departmentinfo.jsx
+++ frontend/v1/src/Components/department/Departmentinfo.jsx
@@ -10,6 +10,7 @@ import { motion } from "framer-motion";
 
 const TimelineItem = ({ icon, title, description, gradient }) => {
   return (
+    <>
     <motion.div
       initial={{ opacity: 0, x: -40 }}
       whileInView={{ opacity: 1, x: 0 }}
@@ -27,9 +28,13 @@ const TimelineItem = ({ icon, title, description, gradient }) => {
       {/* Content */}
       <div>
         <h3 className="text-2xl font-bold text-gray-900 mb-2">{title}</h3>
-        <p className="text-gray-600 text-lg leading-relaxed">{description}</p>
+        
       </div>
     </motion.div>
+    <div>
+      <p className="text-gray-600 text-lg leading-relaxed">{description}</p>
+    </div>
+    </>
   );
 };
 
@@ -40,9 +45,9 @@ const HodSection = () => {
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8 }}
       viewport={{ once: true }}
-      className="relative rounded-3xl bg-white shadow-lg p-8 md:p-12"
+      className="relative rounded-3xl w-[390px] md:w-[1200px] bg-white shadow-lg  p-8 md:p-12"
     >
-      <div className="flex items-center gap-3 mb-6">
+      <div className="flex items-center gap-3  mb-6">
         <span className="bg-gradient-to-br from-purple-600 to-indigo-600 text-white p-3 rounded-xl shadow-md">
           <FaChalkboardTeacher className="text-xl" />
         </span>
@@ -84,7 +89,7 @@ const HodSection = () => {
 
 const VisionMissionHOD = ({ vision, mission }) => {
   return (
-    <section className="py-20 px-6 max-w-6xl mx-auto space-y-20">
+    <section className="py-20  max-w-6xl mx-auto space-y-20">
       {/* Intro */}
       <div className="text-center mb-12">
         <motion.h2
