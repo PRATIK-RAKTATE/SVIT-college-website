67e67be3b5f96c4c7753bb51031a09ec4202b92f
PRATIK-RAKTATE <rhutikraktate@gmail.com>
2025-09-08 12:27:30 +0530

    fix: remove sitemap from home nav

--- frontend/v1/src/Components/HomeNav.jsx
+++ frontend/v1/src/Components/HomeNav.jsx
@@ -264,7 +264,7 @@ export default function HomeNav() {
         <NavLink to="Examination" className={({isActive}) => `${isActive ? "text-red-500" : "text-black"}`}>Examination</NavLink>
         <NavLink to="/contactus" className={({isActive}) => `${isActive ? "text-red-500" : "text-black"}`}>Contact us</NavLink>
         <NavLink to="/about" className={({isActive}) => `${isActive ? "text-red-500" : "text-black"}`}>About us</NavLink>
-        <NavLink to="/sm" className={({isActive}) => `${isActive ? "text-red-500" : "text-black"}`}>Site map</NavLink>
+        {/* <NavLink to="/sm" className={({isActive}) => `${isActive ? "text-red-500" : "text-black"}`}>Site map</NavLink> */}
       </div>
 
       {/* Mobile Menu Button */}
