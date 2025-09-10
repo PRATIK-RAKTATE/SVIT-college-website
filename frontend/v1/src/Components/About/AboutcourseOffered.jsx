f56e9322a22c7b4519a8e153e6e26cac9ace920c
Yogesh chavan <yogeshchavan1209@gmail.com>
2025-09-10 17:29:36 +0530

    fix: about component

--- frontend/v1/src/Components/About/AboutcourseOffered.jsx
+++ frontend/v1/src/Components/About/AboutcourseOffered.jsx
@@ -10,7 +10,7 @@ export default function CoursesTable() {
   ];
 
   return (
-    <div className="bg-white rounded-2xl shadow-2xl overflow-hidden md:w-full ml-4 w-[400px] md:max-w-236 max-h-126 mt-1">
+    <div className="bg-white rounded-2xl shadow-2xl overflow-hidden md:w-full md:mt-14 ml-4 w-[400px] md:max-w-236 max-h-158 mt-1">
       <h2 className="text-2xl font-extrabold text-center py-3 bg-gradient-to-r from-indigo-600 to-purple-600 text-white tracking-wide">
         Courses Offered
       </h2>
@@ -37,7 +37,7 @@ export default function CoursesTable() {
                 i % 2 === 0 ? "bg-white" : "bg-gray-50"
               } hover:bg-indigo-50`}
             >
-              <td className="px-4 py-6 text-gray-800 font-medium">{sr}</td>
+              <td className="px-4 py-6 md:py-11 text-gray-800 font-medium">{sr}</td>
               <td className="px-4 py-6 text-gray-900">{course}</td>
               <td className="px-4 py-6 text-gray-800 font-medium">{intake}</td>
             </tr>
