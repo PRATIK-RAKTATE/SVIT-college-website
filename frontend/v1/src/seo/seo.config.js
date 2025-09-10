4de17c4131fef298e7286eceaefb17cf6fc72259
PRATIK-RAKTATE <rhutikraktate@gmail.com>
2025-09-10 12:01:14 +0530

    seo(routes): optimize routes in the seoconfig

--- frontend/v1/src/seo/seo.config.js
+++ frontend/v1/src/seo/seo.config.js
@@ -15,21 +15,21 @@ const SEO_CONFIG = {
       url: "https://svitnashik.in/department/computer",
       image: "https://pravaraengg.org.in/assets/img/PRES.png",
     },
-    computerEngineering: {
+    "computer-engineering": {
       title: "Computer Engineering Department | SVIT Nashik",
       description:
         "Explore Computer Engineering at SVIT. Faculty, labs, syllabus, placements, and more.",
       url: "https://svitnashik.in/department/computer",
       image: "https://pravaraengg.org.in/assets/img/PRES.png",
     },
-    informationTechnology: {
+    "information-technology": {
       title: "it | SVIT Nashik",
       description:
         "Explore Information Technology Engineering at SVIT. Faculty, labs, syllabus, placements, and more.",
       url: "https://svitnashik.in",
       image: "https://pravaraengg.org.in/assets/img/PRES.png",
     },
-    electronicsAndComputerEngineering: {
+    "electronics-and-computer-engineering": {
       title: "Electronics and Computer Engineering | SVIT Nashik",
       description:
         "Explore Electronica and Computer Engineering at SVIT. Faculty, labs, syllabus, placements, and more.",
