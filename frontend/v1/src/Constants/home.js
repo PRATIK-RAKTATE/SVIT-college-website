8359f8bbf284602300bc4324538b3121d2b8678f
PRATIK-RAKTATE <rhutikraktate@gmail.com>
2025-09-11 21:30:57 +0530

    chore: change local home carousel with cdn

--- frontend/v1/src/Constants/home.js
+++ frontend/v1/src/Constants/home.js
@@ -1,24 +1,19 @@
-import img1 from "../assets/carsusel/svit-building-1.webp";
-import img2 from "../assets/carsusel/svit-building-2.webp";
-import img3 from "../assets/carsusel/svit-building-3.webp";
-import img4 from "../assets/carsusel/svit-building-4.webp";
-
 const home = {
   carousel: [
     {
-      img: img1,
+      img: "https://res.cloudinary.com/difqniecb/image/upload/v1757604276/svit-building-1_w2yuwq.webp",
       text: "Welcome to SVIT",
     },
     {
-      img: img2,
+      img: "https://res.cloudinary.com/difqniecb/image/upload/v1757604276/svit-building-2_mw9fbt.webp",
       text: "Empowering Youth from Rural Areas",
     },
     {
-      img: img3,
+      img: "https://res.cloudinary.com/difqniecb/image/upload/v1757604280/svit-building-3_euq15c.webp",
       text: "Surrounded by Holy Mountains",
     },
     {
-      img: img4,
+      img: "https://res.cloudinary.com/difqniecb/image/upload/v1757604276/svit-building-4_ou6d5f.webp",
       text: "Shaping Future Engineers",
     },
   ],
