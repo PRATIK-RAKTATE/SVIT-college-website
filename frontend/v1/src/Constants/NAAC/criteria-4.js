38c559cdd5ddd796c0e4bd98926f7f12ab81698f
Pravin <pravinpagar290@gmail.com>
2025-09-13 23:33:06 +0530

    add address to criteria 2, 4, 5, 6

--- frontend/v1/src/Constants/NAAC/criteria-4.js
+++ frontend/v1/src/Constants/NAAC/criteria-4.js
@@ -8,12 +8,12 @@ export const criteria_4 = [
         description: "The institution has adequate facilities for teaching- learning. viz., classrooms, laboratories, computing equipment, etc.",
         type: "QlM",
         downloads: [
-          { name: "4.1.1_Adequate_Facilities_for_Teaching-Learning", link: "../NAAC/Criteria_4/4.1.1_Adequate_Facilities_for_Teaching-Learning.pdf" },
-          { name: "4.1.1_Infrastructure And Physical Facilities_2020-21", link: "../NAAC/Criteria_4/4.1.1_Infrastructure And Physical Facilities_2020-21.pdf" },
-          { name: "4.1.1_Infrastructure And Physical Facilities_2021-22", link: "../NAAC/Criteria_4/4.1.1_Infrastructure And Physical Facilities_2021-22.pdf" },
-          { name: "4.1.1_Infrastructure And Physical Facilities_2022-23", link: "../NAAC/Criteria_4/4.1.1_Infrastructure And Physical Facilities_2022-23.pdf" },
-          { name: "Application Report PART-1_2022-23", link: "../NAAC/Criteria_4/Application Report PART-1_2022-23.pdf" },
-          { name: "4.1.1_adequate_facilities_for_teaching-learning_2023-24", link: "../NAAC/Criteria_4/4.1.1_adequate_facilities_for_teaching-learning_2023-24.pdf" }
+          { name: "4.1.1_Adequate_Facilities_for_Teaching-Learning", link: "../../../public/NAAC/criteria 4/4.1.1/4.1.1_adequate_facilities_for_teaching-learning.pdf" },
+          { name: "4.1.1_Infrastructure And Physical Facilities_2020-21", link: "../../../public/NAAC/criteria 4/4.1.1/4.1.1_infrastructure_and_physical_facilities.pdf" },
+          { name: "4.1.1_Infrastructure And Physical Facilities_2021-22", link: "../../../public/NAAC/criteria 4/4.1.1/4.1.1_infrastructure_and_physical_facilities_2021-22.pdf" },
+          { name: "4.1.1_Infrastructure And Physical Facilities_2022-23", link: "../../../public/NAAC/criteria 4/4.1.1/4.1_Physical_Facilities_2022-23.pdf" },
+          { name: "Application Report PART-1_2022-23", link: "../../../public/NAAC/criteria 4/4.1.1/Application Report PART-1_2022-23.pdf" },
+          { name: "4.1.1_adequate_facilities_for_teaching-learning_2023-24", link: "../../../public/NAAC/criteria 4/4.1.1/4.1.1_adequate_facilities_for_teaching-learning_2023-24.pdf" }
         ]
       },
       {
@@ -21,9 +21,9 @@ export const criteria_4 = [
         description: "The institution has adequate facilities for sports, games (indoor, outdoor) gymnasium, yoga centre etc. and cultural activities",
         type: "QlM",
         downloads: [
-          { name: "4.1.2_Adequate_Facilities", link: "../NAAC/Criteria_4/4.1.2_Adequate_Facilities.pdf" },
-          { name: "4.1.2_Adequate_Facilities_2020-21", link: "../NAAC/Criteria_4/4.1.2_Adequate_Facilities_2020-21.pdf" },
-          { name: "4.1.2_Adequate_Facilities_2023-24", link: "../NAAC/Criteria_4/4.1.2_Adequate_Facilities_2023-24.pdf" }
+          { name: "4.1.2_Adequate_Facilities", link: "../../../public/NAAC/criteria 4/4.1.2/4.1.2_adequate_facilities.pdf" },
+          { name: "4.1.2_Adequate_Facilities_2020-21", link: "../../../public/NAAC/criteria 4/4.1.2/4.1.2_Adequate_Facilities_2020-21.pdf" },
+          { name: "4.1.2_Adequate_Facilities_2023-24", link: "../../../public/NAAC/criteria 4/4.1.2/4.1.2_Adequate_Facilities_2023-24.pdf" }
         ]
       },
       {
@@ -31,11 +31,11 @@ export const criteria_4 = [
         description: "Percentage of classrooms and seminar halls with ICT - enabled facilities such as smart class, LMS, etc",
         type: "QlM",
         downloads: [
-          { name: "4.1.3_ClassRoom_With_LCD_ICT_WiFiLAN_Facilities", link: "../NAAC/Criteria_4/4.1.3_ClassRoom_With_LCD_ICT_WiFiLAN_Facilities.pdf" },
-          { name: "4.1.3_ClassRoom_With_ICT_Facilities_PO", link: "../NAAC/Criteria_4/4.1.3_ClassRoom_With_ICT_Facilities_PO.pdf" },
-          { name: "4.1.3_Geotagged_Photos_of_ClassRooms_and_Seminar Halls_With_ICT_Facilities", link: "../NAAC/Criteria_4/4.1.3_Geotagged_Photos_of_ClassRooms_and_Seminar Halls_With_ICT_Facilities.pdf" },
-          { name: "4.1.3_ICT_Facility_2021-22", link: "../NAAC/Criteria_4/4.1.3_ICT_Facility_2021-22.pdf" },
-          { name: "4.1.3_ClassRoom_With_ICT_Facilities_PO_2023-24", link: "../NAAC/Criteria_4/4.1.3_ClassRoom_With_ICT_Facilities_PO_2023-24.pdf" }
+          { name: "4.1.3_ClassRoom_With_LCD_ICT_WiFiLAN_Facilities", link: "../../../public/NAAC/criteria 4/4.1.3/4.1.3_ClassRoom_WithLCDFacility.pdf" },
+          { name: "4.1.3_ClassRoom_With_ICT_Facilities_PO", link: "" },
+          { name: "4.1.3_Geotagged_Photos_of_ClassRooms_and_Seminar Halls_With_ICT_Facilities", link: "../../../public/NAAC/criteria 4/4.1.3/4.1.3.1. Number_of_classrooms_and _seminar_halls_with_ICT_facilities.pdf" },
+          { name: "4.1.3_ICT_Facility_2021-22", link: "../../../public/NAAC/criteria 4/4.1.3/4.1.3_ICT_Facility_2021-22.pdf" },
+          { name: "4.1.3_ClassRoom_With_ICT_Facilities_PO_2023-24", link: "../../../public/NAAC/criteria 4/4.1.3/4.1.3_ClassRoom_With_ICT_Facilities_PO_2023-24.pdf" }
         ]
       },
       {
@@ -43,7 +43,7 @@ export const criteria_4 = [
         description: "Average percentage of budget allocation, excluding salary for infrastructure augmentation during the last five years.",
         type: "QlM",
         downloads: [
-          { name: "4.1.4_Budget_Allocation_Excluding_Salary", link: "../NAAC/Criteria_4/4.1.4_Budget_Allocation_Excluding_Salary.pdf" }
+          { name: "4.1.4_Budget_Allocation_Excluding_Salary", link: "../../../public/NAAC/criteria 4/4.1.4 _budget_allocation_excluding_salary.pdf" }
         ]
       }
     ]
@@ -57,9 +57,9 @@ export const criteria_4 = [
         description: "Library is automated using Integrated Library Management System (ILMS)",
         type: "QlM",
         downloads: [
-          { name: "4.2.1_Integrated_Library_Management_System", link: "../NAAC/Criteria_4/4.2.1_Integrated_Library_Management_System.pdf" },
-          { name: "4.2.1_Integrated_Library_Management_System_2020-21", link: "../NAAC/Criteria_4/4.2.1_Integrated_Library_Management_System_2020-21.pdf" },
-          { name: "4.2.1_Library_is_automated_using_ILMS_2023-24", link: "../NAAC/Criteria_4/4.2.1_Library_is_automated_using_ILMS_2023-24.pdf" }
+          { name: "4.2.1_Integrated_Library_Management_System", link: "../../../public/NAAC/criteria 4/4.2.1/4.2.1_Integrated_Library_Management_System.pdf" },
+          { name: "4.2.1_Integrated_Library_Management_System_2020-21", link: "../../../public/NAAC/criteria 4/4.2.1/4.2.1_Library_As_a_Learning_Resource_2020-21.pdf" },
+          { name: "4.2.1_Library_is_automated_using_ILMS_2023-24", link: "../../../public/NAAC/criteria 4/4.2.1/4.2.1_Library_is_automated_using_ILMS_2023-24.pdf" }
         ]
       },
       {
@@ -67,7 +67,7 @@ export const criteria_4 = [
         description: "Collection of rare books, manuscripts, special reports or any other knowledge resources for library enrichment",
         type: "QlM",
         downloads: [
-          { name: "4.2.2_Collection_Rare_Books_Project and Thesis", link: "../NAAC/Criteria_4/4.2.2_Collection_Rare_Books_Project and Thesis.pdf" }
+          { name: "4.2.2_Collection_Rare_Books_Project and Thesis", link: "../../../public/NAAC/criteria 4/4.2.2_Collection_Rare_Books.pdf" }
         ]
       },
       {
@@ -75,9 +75,9 @@ export const criteria_4 = [
         description: "Does the institution have the following:",
         type: "QlM",
         downloads: [
-          { name: "4.2.3_E-Journals_Subscription", link: "../NAAC/Criteria_4/4.2.3_E-Journals_Subscription.pdf" },
-          { name: "4.2.3_e-Books_Subscription", link: "../NAAC/Criteria_4/4.2.3_e-Books_Subscription.pdf" },
-          { name: "4.2.3_Database & Membership", link: "../NAAC/Criteria_4/4.2.3_Database & Membership.pdf" }
+          { name: "4.2.3_E-Journals_Subscription", link: "../../../public/NAAC/criteria 4/4.2.3/4.2.3_E_Journals_Subscription.pdf" },
+          { name: "4.2.3_e-Books_Subscription", link: "../../../public/NAAC/criteria 4/4.2.3/4.2.3_e_Books_Subscription.pdf" },
+          { name: "4.2.3_Database & Membership", link: "../../../public/NAAC/criteria 4/4.2.3/4.2.3_Database_Membership.pdf" }
         ]
       },
       {
@@ -85,10 +85,10 @@ export const criteria_4 = [
         description: "Average annual expenditure for purchase of books and journals during the last five years (INR in Lakhs)",
         type: "QlM",
         downloads: [
-          { name: "4.2.4_Library_BILL_SUMMARY", link: "../NAAC/Criteria_4/4.2.4_Library_BILL_SUMMARY.pdf" },
-          { name: "4.2.4_Library_Audited Statement of Account", link: "../NAAC/Criteria_4/4.2.4_Library_Audited Statement of Account.pdf" },
-          { name: "4.2.4_Library_Budget", link: "../NAAC/Criteria_4/4.2.4_Library_Budget.pdf" },
-          { name: "4.2.4_Audited_Statement", link: "../NAAC/Criteria_4/4.2.4_Audited_Statement.pdf" }
+          { name: "4.2.4_Library_BILL_SUMMARY", link: "../../../public/NAAC/criteria 4/4.2.4/4.2.4_Library_BILL_SUMMARY.pdf" },
+          { name: "4.2.4_Library_Audited Statement of Account", link: "../../../public/NAAC/criteria 4/4.2.4/4.2.4_Library_Audited_Statement_of_Account.pdf" },
+          { name: "4.2.4_Library_Budget", link: "../../../public/NAAC/criteria 4/4.2.4/4.2.4_Library_Budget.pdf" },
+          { name: "4.2.4_Audited_Statement", link: "../../../public/NAAC/criteria 4/4.2.4/4.2.4.Audited_Statement.pdf" }
         ]
       },
       {
@@ -96,9 +96,9 @@ export const criteria_4 = [
         description: "Availability of remote access to e-resources of the library",
         type: "QlM",
         downloads: [
-          { name: "4.2.5_E_Resources", link: "../NAAC/Criteria_4/4.2.5_E_Resources.pdf" },
-          { name: "4.2.5_Details of Contact person", link: "../NAAC/Criteria_4/4.2.5_Details of Contact person.pdf" },
-          { name: "4.2.5_Connectivity Bandwidth available", link: "../NAAC/Criteria_4/4.2.5_Connectivity Bandwidth available.pdf" }
+          { name: "4.2.5_E_Resources", link: "../../../public/NAAC/criteria 4/4.2.5/4.2.5_E_Resources.pdf" },
+          { name: "4.2.5_Details of Contact person", link: "../../../public/NAAC/criteria 4/4.2.5/4.2.5_Detail_of_Contact_person.pdf" },
+          { name: "4.2.5_Connectivity Bandwidth available", link: "../../../public/NAAC/criteria 4/4.2.5/4.2.5_Connectivity_Bandwidth_available.pdf" }
         ]
       },
       {
@@ -106,7 +106,7 @@ export const criteria_4 = [
         description: "Percentage per day usage of library by teachers and students",
         type: "QlM",
         downloads: [
-          { name: "4.2.6_Library_Usage", link: "../NAAC/Criteria_4/4.2.6_Library_Usage.pdf" }
+          { name: "4.2.6_Library_Usage", link: "../../../public/NAAC/criteria 4/4.2.6_Library_Usage.pdf" }
         ]
       }
     ]
@@ -120,11 +120,11 @@ export const criteria_4 = [
         description: "Institution frequently updates its IT facilities including Wi-Fi",
         type: "QlM",
         downloads: [
-          { name: "4.3.1_IT_Facilities_Including_Wi-Fi", link: "../NAAC/Criteria_4/4.3.1_IT_Facilities_Including_Wi-Fi.pdf" },
-          { name: "4.3.1_Internet_PO_2020-21", link: "../NAAC/Criteria_4/4.3.1_Internet_PO_2020-21.pdf" },
-          { name: "4.3.1_IT_facilities_including_Wi-Fi_2021-22", link: "../NAAC/Criteria_4/4.3.1_IT_facilities_including_Wi-Fi_2021-22.pdf" },
-          { name: "4.3.1_IT_facilities_including_Wi-Fi_2022-23", link: "../NAAC/Criteria_4/4.3.1_IT_facilities_including_Wi-Fi_2022-23.pdf" },
-          { name: "4.3.1_Institution_frequently_updates_its_IT_facilities_2023-24", link: "../NAAC/Criteria_4/4.3.1_Institution_frequently_updates_its_IT_facilities_2023-24.pdf" }
+          { name: "4.3.1_IT_Facilities_Including_Wi-Fi", link: "../../../public/NAAC/criteria 4/4.3.1/4.3.1_IT_facilities_including_Wi-Fi.pdf" },
+          { name: "4.3.1_Internet_PO_2020-21", link: "../../../public/NAAC/criteria 4/4.3.1/4.3.1_Internet_PO_2020-21.pdf" },
+          { name: "4.3.1_IT_facilities_including_Wi-Fi_2021-22", link: "../../../public/NAAC/criteria 4/4.3.1/4.3.1_IT_facilities_including_Wi-Fi_2021-22.pdf" },
+          { name: "4.3.1_IT_facilities_including_Wi-Fi_2022-23", link: "../../../public/NAAC/criteria 4/4.3.1/4.3.1_IT_facilities_including_Wi-Fi_2022-23.pdf" },
+          { name: "4.3.1_Institution_frequently_updates_its_IT_facilities_2023-24", link: "../../../public/NAAC/criteria 4/4.3.1/4.3.1_Institution_frequently_updates_its_IT_facilities_2023-24.pdf" }
         ]
       },
       {
@@ -132,9 +132,9 @@ export const criteria_4 = [
         description: "Student - Computer ratio",
         type: "QlM",
         downloads: [
-          { name: "4.3.2_Computer_Purchase", link: "../NAAC/Criteria_4/4.3.2_Computer_Purchase.pdf" },
-          { name: "4.3.2_Student_Computer_Ratio", link: "../NAAC/Criteria_4/4.3.2_Student_Computer_Ratio.pdf" },
-          { name: "4.3.2_Students Roll Call list 2017-18", link: "../NAAC/Criteria_4/4.3.2_Students Roll Call list 2017-18.pdf" }
+          { name: "4.3.2_Computer_Purchase", link: "../../../public/NAAC/criteria 4/4.3.2/4.3.2_Computer_Purchase.pdf" },
+          { name: "4.3.2_Student_Computer_Ratio", link: "../../../public/NAAC/criteria 4/4.3.2/4.3.2_Student_Computer_Ratio1.pdf" },
+          { name: "4.3.2_Students Roll Call list 2017-18", link: "../../../public/NAAC/criteria 4/4.3.2/4.3.2_Students_Roll_list_2017-18.pdf" }
         ]
       },
       {
@@ -142,8 +142,8 @@ export const criteria_4 = [
         description: "Available bandwidth of internet connection in the Institution (Lease line)",
         type: "QlM",
         downloads: [
-          { name: "4.3.3_Internet_Bandwidth", link: "../NAAC/Criteria_4/4.3.3_Internet_Bandwidth.pdf" },
-          { name: "4.3.3_Internet_BW_Upgradation", link: "../NAAC/Criteria_4/4.3.3_Internet_BW_Upgradation.pdf" }
+          { name: "4.3.3_Internet_Bandwidth", link: "../../../public/NAAC/criteria 4/4.3.3/4.3.3_Internet_Bandwidth.pdf" },
+          { name: "4.3.3_Internet_BW_Upgradation", link: "../../../public/NAAC/criteria 4/4.3.3/4.3.3_Internet_BW_Upgradation.pdf" }
         ]
       },
       {
@@ -151,7 +151,7 @@ export const criteria_4 = [
         description: "Facilities for e-content development such as Media Centre, Recording facility, Lecture Capturing System (LCS)",
         type: "QlM",
         downloads: [
-          { name: "4.3.4_Facilities_For_e-Content_Development", link: "../NAAC/Criteria_4/4.3.4_Facilities_For_e-Content_Development.pdf" }
+          { name: "4.3.4_Facilities_For_e-Content_Development", link: "../../../public/NAAC/criteria 4/4.3.4_e_Contents.pdf" }
         ]
       }
     ]
@@ -165,7 +165,7 @@ export const criteria_4 = [
         description: "Average Expenditure incurred on maintenance of physical facilities and academic support facilities excluding salary component, as a percentage during the last five years",
         type: "QlM",
         downloads: [
-          { name: "4.4.1_Audited_Statment", link: "../NAAC/Criteria_4/4.4.1_Audited_Statment.pdf" }
+          { name: "4.4.1_Audited_Statment", link: "../../../public/NAAC/criteria 4/4.4.1_Audited_Statment.pdf" }
         ]
       },
       {
@@ -173,13 +173,13 @@ export const criteria_4 = [
         description: "There are established systems and procedures for maintaining and utilizing physical, academic and support facilities - laboratory, library, sports complex, computers, classrooms etc.",
         type: "QlM",
         downloads: [
-          { name: "4.4.2_Maintaining_and_Utilization", link: "../NAAC/Criteria_4/4.4.2_Maintaining_and_Utilization.pdf" },
-          { name: "4.4.2_Maintaining_and_Utilization(2018-19)", link: "../NAAC/Criteria_4/4.4.2_Maintaining_and_Utilization(2018-19).pdf" },
-          { name: "4.4.2_Maitaninance_policies", link: "../NAAC/Criteria_4/4.4.2_Maitaninance_policies.pdf" },
-          { name: "4.4.2_Maintenance_2020-21", link: "../NAAC/Criteria_4/4.4.2_Maintenance_2020-21.pdf" },
-          { name: "4.4.2_procedures_for_maintaining_facility_2021-22", link: "../NAAC/Criteria_4/4.4.2_procedures_for_maintaining_facility_2021-22.pdf" },
-          { name: "4.4.2_procedures_for_maintaining_facility_2022-23", link: "../NAAC/Criteria_4/4.4.2_procedures_for_maintaining_facility_2022-23.pdf" },
-          { name: "4.4.2_procedures_for_maintaining_facility_2023-24", link: "../NAAC/Criteria_4/4.4.2_procedures_for_maintaining_facility_2023-24.pdf" }
+          { name: "4.4.2_Maintaining_and_Utilization", link: "../../../public/NAAC/criteria 4/4.4.2/4.4.2_maintaining_and_utilization.pdf" },
+          { name: "4.4.2_Maintaining_and_Utilization(2018-19)", link: "../../../public/NAAC/criteria 4/4.4.2/4.4.2_maintaining_and_utilization_2018-19.pdf" },
+          { name: "4.4.2_Maitaninance_policies", link: "../../../public/NAAC/criteria 4/4.4.2/4.4.2_facilities.pdf" },
+          { name: "4.4.2_Maintenance_2020-21", link: "../../../public/NAAC/criteria 4/4.4.2/4.4.2_Maintenance_2020-21.pdf" },
+          { name: "4.4.2_procedures_for_maintaining_facility_2021-22", link: "../../../public/NAAC/criteria 4/4.4.2/4.4.2_procedures_for_maintaining_facility_2021-22.pdf" },
+          { name: "4.4.2_procedures_for_maintaining_facility_2022-23", link: "../../../public/NAAC/criteria 4/4.4.2/4.4.2_procedures_for_maintaining_facility_2022-23.pdf" },
+          { name: "4.4.2_procedures_for_maintaining_facility_2023-24", link: "../../../public/NAAC/criteria 4/4.4.2/4.4.2_procedures_for_maintaining_facility_2023-24.pdf" }
         ]
       }
     ]
